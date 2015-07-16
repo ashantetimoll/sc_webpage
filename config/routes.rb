af9003a04a8b288bfa986b52e6791fb46df5252d
Ashante Timoll <ashantetimoll@gmail.com>
2015-07-16 15:58:32 -0400

    added devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
  
   devise_for :users
   get 'about' => 'welcome#about'
+  
   root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150715200204) do
+ActiveRecord::Schema.define(version: 20150716195115) do
+
+  create_table "contact_us", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
